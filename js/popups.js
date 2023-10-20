3a0a391722186790259a28cc74cd9aaf7c29fa4f
DiogoMoreiraaa <Albinho1907@gmail.com>
2023-10-19 21:10:40 -0300

    .

---
+++ js/popups.js
@@ -0,0 +1,9 @@
+function exibirPopup() {
+    // Exibe o popup definindo o estilo 'display' para 'block'
+    document.getElementById('popup').style.display = 'block';
+}
+
+function fecharPopup() {
+    // Fecha o popup definindo o estilo 'display' para 'none'
+    document.getElementById('popup').style.display = 'none';
+}
